29696adb1e56f12060b85b17aea01a3352229380
Mahmoud Abumandour <mam47@sfu.ca>
2024-12-12 18:53:11 -0800

    More init tests

--- test/init_tests.cpp
+++ test/init_tests.cpp
@@ -5,6 +5,7 @@
 TEST(TensorInitialization, DefaultConstructor)
 {
     Tensor<int> tensor;
+    EXPECT_EQ(tensor.shape()[0], 0);
     ASSERT_EQ(tensor.size(), 0);
 }
 
@@ -12,13 +13,24 @@ TEST(TensorInitialization, ConstructorWithShape)
 {
     auto tensor1 = Tensor<int>(nullptr, { 2, 3 });
     EXPECT_EQ(tensor1.size(), 6);
+    EXPECT_EQ(tensor1.shape()[0], 2);
+    EXPECT_EQ(tensor1.shape()[1], 3);
 
     auto tensor2 = Tensor<int>(nullptr, { 2, 3, 4 });
     EXPECT_EQ(tensor2.size(), 24);
+    EXPECT_EQ(tensor2.shape()[0], 2);
+    EXPECT_EQ(tensor2.shape()[1], 3);
+    EXPECT_EQ(tensor2.shape()[2], 4);
 
     auto tensor3 = Tensor<int>(nullptr, { 2, 3, 4, 5 });
     EXPECT_EQ(tensor3.size(), 120);
+    EXPECT_EQ(tensor3.shape()[0], 2);
+    EXPECT_EQ(tensor3.shape()[1], 3);
+    EXPECT_EQ(tensor3.shape()[2], 4);
+    EXPECT_EQ(tensor3.shape()[3], 5);
+}
 
-    // This tensor's shape has more than 4 dimensions, which is not supported
-    EXPECT_THROW(Tensor<int>(nullptr, { 2, 3, 4, 5, 6 }), std::invalid_argument);
+TEST(TensorConstructorTest, InvalidShape) {
+    std::initializer_list<size_t> shape = {1, 2, 3, 4, 5};
+    EXPECT_THROW(Tensor<int>(nullptr, shape), std::invalid_argument);
 }
