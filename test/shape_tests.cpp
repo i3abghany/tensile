a1bae763ab918acc6657fb342dbb167e3754aa79
Mahmoud Abumandour <mam47@sfu.ca>
2024-12-15 22:38:55 -0800

    Test matmul 2d shape compatibility

--- test/shape_tests.cpp
+++ test/shape_tests.cpp
@@ -3,6 +3,7 @@
 #include "tensor.h"
 #include "test_utils.h"
 
+using std::array;
 using std::make_tuple;
 using std::pair;
 using std::string;
@@ -10,7 +11,8 @@ using std::tuple;
 using std::vector;
 using Tensile::Tensor;
 
-class TensorShapeCompatibilityTest : public ::testing::TestWithParam<tuple<vector<size_t>, vector<size_t>, bool>> {
+class TensorElementwiseShapeCompatibilityTest
+    : public ::testing::TestWithParam<tuple<vector<size_t>, vector<size_t>, bool>> {
 protected:
     Tensor<int>* tensor_a;
     Tensor<int>* tensor_b;
@@ -40,7 +42,7 @@ protected:
     }
 };
 
-TEST_P(TensorShapeCompatibilityTest, ShapeCompatibility)
+TEST_P(TensorElementwiseShapeCompatibilityTest, ShapeCompatibility)
 {
     if (legal)
         ASSERT_TRUE(Tensor<int>::shape_compat(*tensor_a, *tensor_b));
@@ -49,7 +51,7 @@ TEST_P(TensorShapeCompatibilityTest, ShapeCompatibility)
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    TensorShapeCompatibilityTests, TensorShapeCompatibilityTest,
+    TensorElementwiseShapeCompatibilityTest, TensorElementwiseShapeCompatibilityTest,
     ::testing::Values(make_tuple(vector<size_t> { 3 }, vector<size_t> { 3 }, true),
                       make_tuple(vector<size_t> { 3 }, vector<size_t> { 1 }, true),
                       make_tuple(vector<size_t> { 1 }, vector<size_t> { 3 }, true),
@@ -104,3 +106,67 @@ INSTANTIATE_TEST_SUITE_P(
                       make_tuple(vector<size_t> { 1, 1, 1, 3 }, vector<size_t> { 4, 2 }, false),
                       make_tuple(vector<size_t> { 1, 2, 1, 4 }, vector<size_t> { 2, 1, 2 }, false),
                       make_tuple(vector<size_t> { 2, 4, 4, 2 }, vector<size_t> { 4, 3, 3 }, false)));
+
+class TensorMatmul2DShapeCompatibilityTest
+    : public ::testing::TestWithParam<tuple<array<size_t, 2>, array<size_t, 2>, bool>> {
+protected:
+    Tensor<int>* tensor_a;
+    Tensor<int>* tensor_b;
+    array<size_t, 2> shape_a;
+    array<size_t, 2> shape_b;
+    size_t len_a;
+    size_t len_b;
+
+    bool legal;
+
+    void SetUp() override
+    {
+        shape_a = std::get<0>(GetParam());
+        shape_b = std::get<1>(GetParam());
+        legal = std::get<2>(GetParam());
+        len_a = flat_size(vector<size_t>(shape_a.begin(), shape_a.end()));
+        len_a = flat_size(vector<size_t>(shape_a.begin(), shape_a.end()));
+
+        tensor_a = new Tensor<int>(nullptr, shape_a);
+        tensor_b = new Tensor<int>(nullptr, shape_b);
+    }
+
+    void TearDown() override
+    {
+        delete tensor_a;
+        delete tensor_b;
+    }
+};
+
+TEST_P(TensorMatmul2DShapeCompatibilityTest, ShapeCompatibility)
+{
+    if (legal)
+        ASSERT_TRUE(Tensor<int>::matmul_compat(*tensor_a, *tensor_b));
+    else
+        ASSERT_FALSE(Tensor<int>::matmul_compat(*tensor_a, *tensor_b));
+}
+
+constexpr auto generate_test_cases()
+{
+    constexpr size_t M = 5;
+    constexpr size_t num_cases = M * M * M * M;
+    array<tuple<array<size_t, 2>, array<size_t, 2>, bool>, num_cases> test_cases;
+
+    size_t index = 0;
+    for (size_t i = 1; i <= M; i++) {
+        for (size_t j = 1; j <= M; j++) {
+            for (size_t k = 1; k <= M; k++) {
+                for (size_t l = 1; l <= M; l++) {
+                    array<size_t, 2> shape_a = { i, j };
+                    array<size_t, 2> shape_b = { k, l };
+                    bool legal = j == k;
+                    test_cases[index++] = make_tuple(shape_a, shape_b, legal);
+                }
+            }
+        }
+    }
+    return test_cases;
+}
+
+INSTANTIATE_TEST_SUITE_P(TensorMatmul2DShapeCompatibilityTest, TensorMatmul2DShapeCompatibilityTest,
+                         ::testing::ValuesIn(generate_test_cases()));
